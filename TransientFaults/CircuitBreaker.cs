e49920739a35c20b131e5bac84ecceee10a9570d
Bila Akpan <bilaakpan@gmail.com>
2019-12-21 22:54:07 -0500

    Add support for netstandard2.1, and Rest if not fit threshold in heal period

--- TransientFaults/CircuitBreaker.cs
+++ TransientFaults/CircuitBreaker.cs
@@ -2,20 +2,16 @@
 using System.Threading;
 using System.Threading.Tasks;
 
-namespace TransientFaults
-{
-    public class CircuitBreaker : ICircuitBreaker
-    {
-        private enum CircuitState
-        {
+namespace TransientFaults {
+    public class CircuitBreaker : ICircuitBreaker {
+        private enum CircuitState {
             Closed = 0,
             Open = 1,
             HardOpen = 2
         }
 
         public readonly ICircuitBreakerConfig _configuration;
-        public CircuitBreaker(ICircuitBreakerConfig configuration)
-        {
+        public CircuitBreaker(ICircuitBreakerConfig configuration) {
             _configuration = configuration ?? throw new ArgumentNullException(nameof(configuration));
         }
 
@@ -23,18 +19,15 @@ namespace TransientFaults
         private DateTime LastTrippedUtc { get; set; }
         private short FailureCount { get; set; }
 
-        private void Trip()
-        {
-            if (FailureCount > _configuration.FailureCount)
-            {
+        private void Trip() {
+            if (FailureCount > _configuration.FailureCount) {
                 LastTrippedUtc = DateTime.UtcNow;
                 State = nameof(CircuitState.Open);
             }
             FailureCount++;
         }
 
-        public void Reset()
-        {
+        public void Reset() {
             State = nameof(CircuitState.Closed);
             FailureCount = 0;
         }
@@ -48,26 +41,27 @@ namespace TransientFaults
         public TResult ExecuteTask<TResult>(Func<TResult> action)
         => ExecuteTaskAsync(_ => Task.FromResult(action()), new CancellationToken()).GetAwaiter().GetResult();
         public Task ExecuteTaskAsync(Action action)
-        => ExecuteTaskAsync(_ =>
-        {
+        => ExecuteTaskAsync(_ => {
             action();
             return Task.FromResult(0);
         }, new CancellationToken());
         public Task<TResult> ExecuteTaskAsync<TResult>(Func<CancellationToken, Task<TResult>> action, CancellationToken token)
         => ExecuteTaskAsync(action, token, null);
 
-        public async Task<TResult> ExecuteTaskAsync<TResult>(Func<CancellationToken, Task<TResult>> action, CancellationToken token, Func<Task<TResult>> circuitOpenValue)
-        {
+        public async Task<TResult> ExecuteTaskAsync<TResult>(Func<CancellationToken, Task<TResult>> action, CancellationToken token, Func<Task<TResult>> circuitOpenValue) {
             if (State == nameof(CircuitState.HardOpen))
                 throw new CircuitBreakerOpenException("The Circuit Breaker is in a Hard Open state");
-            try
-            {
+            try {
                 token.ThrowIfCancellationRequested();
-                if (IsClosed)
-                    return await action(token).ConfigureAwait(false);
+                if (IsClosed) {
+                    var task = await action(token).ConfigureAwait(false);
 
-                if (LastTrippedUtc.TimeOfDay + _configuration.HealPeriod < DateTime.UtcNow.TimeOfDay)
-                {
+                    if (FailureCount > 0 && LastTrippedUtc.TimeOfDay + _configuration.HealPeriod < DateTime.UtcNow.TimeOfDay) {
+                        Reset();
+                    }
+                    return task;
+                }
+                if (LastTrippedUtc.TimeOfDay + _configuration.HealPeriod < DateTime.UtcNow.TimeOfDay) {
                     var actionResult = await action(token).ConfigureAwait(false);
                     Reset();
                     return actionResult;
@@ -77,11 +71,10 @@ namespace TransientFaults
             catch (CircuitBreakerOpenException) { throw; }
             catch { Trip(); }
 
-            if (circuitOpenValue != null)
-            {
+            if (circuitOpenValue != null) {
                 return await circuitOpenValue();
             }
-            throw new Exception($"Method failed to eacuate in Circuit Breaker: {action}");
+            throw new Exception($"Method failed to execute in Circuit Breaker: {action}");
         }
     }
 }
